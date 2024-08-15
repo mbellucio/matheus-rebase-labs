4ebd49434c41fbc2999ed401e9109c5e7f18e901
Matheus Bellucio <mbelluciocoding@gmail.com>
2024-08-15 16:38:26 -0300

    Job deletes temp file after done

--- sidekiq/csv_job.rb
+++ sidekiq/csv_job.rb
@@ -1,5 +1,6 @@
 require 'sidekiq'
 require './api/medical_exam'
+require 'fileutils'
 
 class CsvJob
   include Sidekiq::Job
@@ -7,6 +8,7 @@ class CsvJob
   def perform(file_path, table_name='medical_exam')
     medical_exam = MedicalExam.new(table_name)
     medical_exam.populate_from_csv(file_path)
+    FileUtils.rm_rf(file_path)
   end
 
 end
